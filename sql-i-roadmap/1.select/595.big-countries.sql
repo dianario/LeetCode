32551bee5a5da51eca8116b3a837424da03421c9
Diana Rios <31250962+dianario@users.noreply.github.com>
2023-10-09 21:29:12 +0100

    Solve 595

--- sql-i-roadmap/1.select/595.big-countries.sql
+++ sql-i-roadmap/1.select/595.big-countries.sql
@@ -6,6 +6,13 @@
 
 -- @lc code=start
 # Write your MySQL query statement below
-
+SELECT
+    name, 
+    population, 
+    area
+FROM 
+    World
+WHERE
+    area >= 3000000 OR population >= 25000000
 -- @lc code=end
 
