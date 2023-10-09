4bda0f6d897ac3a2d41407de7f1799a9edd80b27
Diana Rios <31250962+dianario@users.noreply.github.com>
2023-10-09 21:09:28 +0100

    Solve 183

--- sql-i-roadmap/1.select/183.customers-who-never-order.sql
+++ sql-i-roadmap/1.select/183.customers-who-never-order.sql
@@ -6,6 +6,15 @@
 
 -- @lc code=start
 # Write your MySQL query statement below
+SELECT 
+    customers.name AS Customers
+FROM 
+    Customers as customers 
+LEFT JOIN
+    Orders as orders
+ON customers.id = orders.customerId
+WHERE 
+ orders.id IS NULL
 
 -- @lc code=end
 
